a0f8c1bfbddde4d38a9be67ebad4e58969653ea9
SethDonohue <donohue.seth@gmail.com>
2018-02-28 19:37:26 -0800

    minor changes

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 require('dotenv').config();
 
 const host = 'www.sethdonohue.com';
-if ((host === window.location.host) && (window.location.protocol !== 'https:')) {
-  window.location.protocol = 'https';
-}
 
 const express = require('express');
 const favicon = require('serve-favicon');
