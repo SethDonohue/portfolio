e78269188041caa8991d99de362b9676a4af5dea
SethDonohue <donohue.seth@gmail.com>
2018-03-07 19:56:16 -0800

    css grids is GREAT! added more transitions, added better code for react image setting

--- src/component/project/index.js
+++ src/component/project/index.js
@@ -7,6 +7,7 @@ import image2 from '../../image/project1ss2.png';
 import image3 from '../../image/project1ss3.png';
 import image4 from '../../image/project1ss4.png';
 import analyticsImg from '../../image/analytics.png';
+import socketImg from '../../image/socket-io.png';
 
 class Project extends Component {
   state = {
@@ -23,65 +24,81 @@ class Project extends Component {
   render() {
     // These are defined for multiple uses for OnMouseOver & onFocus for
     // Accessability as determined by a11y standards (https://a11yproject.com/)
-
-    const defaultMouseOut = () => { this.setState({ imgSrc: image1 }); };
-    const featureOneImgSrcSet = () => { this.setState({ imgSrc: image2 }); };
-    const featureTwoImgSrcSet = () => { this.setState({ imgSrc: image3 }); };
-    const featureThreeImgSrcSet = () => { this.setState({ imgSrc: image4 }); };
+    const defaultMouseOut = () => {
+      this.setState({
+        imgSrc: image1,
+        featureSrc: analyticsImg,
+      });
+    };
+    const featureOneImgSrcSet = () => {
+      this.setState({
+        imgSrc: image2,
+        featureSrc: socketImg,
+      });
+    };
+    const featureTwoImgSrcSet = () => {
+      this.setState({
+        imgSrc: image3,
+        featureSrc: socketImg,
+      });
+    };
+    const featureThreeImgSrcSet = () => {
+      this.setState({
+        imgSrc: image4,
+        featureSrc: socketImg,
+      });
+    };
 
     return (
       <div className="project-item">
-        <div className="container">
-          <img className="element" src={this.state.imgSrc} alt="" />
-        </div>
-        <div className="container">       
-          <ul className="feature-list">
-            <li 
-              className="feature"
-              id="featureOne"
-              onMouseOut={defaultMouseOut}
-              onMouseOver={featureOneImgSrcSet}
-              onFocus={featureOneImgSrcSet}
-              onBlur={defaultMouseOut}
-            > 
-              <p>
-                <img className="feature-img" src={this.state.featureSrc} alt="" />
+        <img className="screenshot" src={this.state.imgSrc} alt="" />
+        
+        <ul className="feature-list">
+          <li 
+            className="feature"
+            id="featureOne"
+            onMouseOut={defaultMouseOut}
+            onMouseOver={featureOneImgSrcSet}
+            onFocus={featureOneImgSrcSet}
+            onBlur={defaultMouseOut}
+          > 
+            <img className="feature-img" src={this.state.featureSrc} alt="" />
+            <p>
               Feature 1 Lorem IpsumFeature One Lorem IpsumFeature 
               One Lorem IpsumFeature One Lorem IpsumFeature One Lorem 
-              </p>
-            </li>
+            </p>
+          </li>
 
-            <li 
-              className="feature"
-              id="featureTwo"
-              onMouseOver={featureTwoImgSrcSet}
-              onMouseOut={defaultMouseOut}
-              onFocus={featureTwoImgSrcSet}
-              onBlur={defaultMouseOut}
-            > 
-              <p>
-                <img className="feature-img" src={this.state.featureSrc} alt="" />
+          <li 
+            className="feature"
+            id="featureTwo"
+            onMouseOver={featureTwoImgSrcSet}
+            onMouseOut={defaultMouseOut}
+            onFocus={featureTwoImgSrcSet}
+            onBlur={defaultMouseOut}
+          > 
+            <img className="feature-img" src={this.state.featureSrc} alt="" />
+            <p>
               Feature 2 Lorem IpsumFeature One Lorem IpsumFeature
               One Lorem IpsumFeature One Lorem IpsumFeature One Lorem
-              </p>
-            </li>
+            </p>
+          </li>
 
-            <li 
-              className="feature"
-              id="featureThree"
-              onMouseOver={featureThreeImgSrcSet}
-              onMouseOut={defaultMouseOut}
-              onFocus={featureThreeImgSrcSet}
-              onBlur={defaultMouseOut}
-            > 
-              <p>
-                <img className="feature-img" src={this.state.featureSrc} alt="" />            
+          <li 
+            className="feature"
+            id="featureThree"
+            onMouseOver={featureThreeImgSrcSet}
+            onMouseOut={defaultMouseOut}
+            onFocus={featureThreeImgSrcSet}
+            onBlur={defaultMouseOut}
+          > 
+            <img className="feature-img" src={this.state.featureSrc} alt="" />            
+            <p>
               Feature 3 Lorem IpsumFeature One Lorem IpsumFeature
               One Lorem IpsumFeature One Lorem IpsumFeature One Lorem
-              </p>
-            </li>
-          </ul>
-        </div>
+            </p>
+          </li>
+        </ul>
       </div>
     );
   }
