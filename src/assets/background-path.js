e6da42528a9b890e005494af9a97df2a1e85edc5
SethDonohue <donohue.seth@gmail.com>
2018-03-19 17:02:03 -0700

    fixed path to work for now

--- src/assets/background-path.js
+++ src/assets/background-path.js
@@ -1,21 +1,21 @@
 // Get a reference to the <path> element
 const path = document.querySelector('#background-path');
-const path2 = document.querySelector('#background-path-2');
+// const path2 = document.querySelector('#background-path-2');
 
 // Get length of <path>
 const pathLength = path.getTotalLength();
-const pathLength2 = path2.getTotalLength();
+// const pathLength2 = path2.getTotalLength();
 
 // Creates very long dash to initially hide the shape
 path.style.strokeDasharray = `${pathLength} ${pathLength}`;
-path2.style.strokeDasharray = `${pathLength2} ${pathLength2}`;
+// path2.style.strokeDasharray = `${pathLength2} ${pathLength2}`;
 
 // Offset dashes so <path> appears hidden completely
 path.style.strokeDashoffset = pathLength;
-path2.style.strokeDashoffset = pathLength2;
+// path2.style.strokeDashoffset = pathLength2;
 
 path.getBoundingClientRect();
-path2.getBoundingClientRect();
+// path2.getBoundingClientRect();
 
 // When the page scrolls...
 window.addEventListener('scroll', (event) => {
@@ -26,10 +26,10 @@ window.addEventListener('scroll', (event) => {
   //(document.documentElement.scrollHeight - document.documentElement.clientHeight);
   
   const drawLength = pathLength * scrollPercentage;
-  const drawLength2 = pathLength2 * scrollPercentage;
+  // const drawLength2 = pathLength2 * scrollPercentage;
 
   path.style.strokeDashoffset = pathLength - drawLength;
-  path2.style.strokeDashoffset = pathLength2 - drawLength2;
+  // path2.style.strokeDashoffset = pathLength2 - drawLength2;
 
   // At end of scrolling make shape complete as
   // the end of the path won't match properly due to dashing, so move dash array
