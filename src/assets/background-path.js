b2e4f3142e08d91d1d83026028df5bff405be702
SethDonohue <donohue.seth@gmail.com>
2018-03-28 18:18:28 -0700

    moved linde drawing to bottom right

--- src/assets/background-path.js
+++ src/assets/background-path.js
@@ -1,44 +1,44 @@
 // Get a reference to the <path> element
-const path = document.querySelector('#background-path');
+// const path = document.querySelector('#background-path');
 const svgText = document.querySelector('#background-text-one');
 
 // Get length of <path>
-const pathLength = path.getTotalLength();
+// const pathLength = path.getTotalLength();
 const svgTextLength = svgText.getTotalLength();
 
 // Creates very long dash to initially hide the shape
-path.style.strokeDasharray = `${pathLength} ${pathLength}`;
+// path.style.strokeDasharray = `${pathLength} ${pathLength}`;
 svgText.style.strokeDasharray = `${svgTextLength} ${svgTextLength}`;
 
 // Offset dashes so <path> appears hidden completely
-path.style.strokeDashoffset = pathLength;
+// path.style.strokeDashoffset = pathLength;
 svgText.style.strokeDashoffset = svgTextLength;
 
-path.getBoundingClientRect();
+// path.getBoundingClientRect();
 svgText.getBoundingClientRect();
 
 // When the page scrolls...
 window.addEventListener('scroll', (event) => {
-  const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / 
-  (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  // const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / 
+  // (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
   const scrollPercentage2 = (document.documentElement.scrollTop + document.body.scrollTop) / 
   (document.documentElement.scrollHeight - document.documentElement.clientHeight);
   
-  const drawLength = pathLength * scrollPercentage;
+  // const drawLength = pathLength * scrollPercentage;
   const drawLength2 = svgTextLength * scrollPercentage2;
 
-  path.style.strokeDashoffset = pathLength - drawLength;
+  // path.style.strokeDashoffset = pathLength - drawLength;
   svgText.style.strokeDashoffset = svgTextLength - drawLength2;
 
   // At end of scrolling make shape complete as
   // the end of the path won't match properly due to dashing, so move dash array
-  if (scrollPercentage >= 0.99) {
-    path.style.strokeDasharray = 'none';
-  } else {
-    path.style.strokeDasharray = `${pathLength} ${pathLength}`;
-  }
-  if (scrollPercentage >= 0.45) {
+  // if (scrollPercentage >= 0.99) {
+    // path.style.strokeDasharray = 'none';
+  // } else {
+    // path.style.strokeDasharray = `${pathLength} ${pathLength}`;
+  // }
+  if (scrollPercentage2 >= 0.45) {
     svgText.style.strokeDasharray = 'none';
   } else {
     svgText.style.strokeDasharray = `${svgTextLength} ${svgTextLength}`;
